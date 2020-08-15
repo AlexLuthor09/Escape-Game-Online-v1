1d2412cdea4ce710bca3de82c1c16be88502642d
AlexLuthor09 <capalex@hotmail.be>
2020-08-15 13:11:27 +0200

    Ajoutez des fichiers projet.

---
+++ Escape Game Online v1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Escape_Game_Online_v1
+{
+    partial class OcarinaBox
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxMP = new System.Windows.Forms.TextBox();
+            this.ValidationButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxName.Font = new System.Drawing.Font("Arial Narrow", 19.8F);
+            this.textBoxName.Location = new System.Drawing.Point(88, 79);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(472, 45);
+            this.textBoxName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F);
+            this.label1.Location = new System.Drawing.Point(85, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nom Utilisateur :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 16.2F);
+            this.label2.Location = new System.Drawing.Point(85, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 32);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mot de passe :";
+            // 
+            // textBoxMP
+            // 
+            this.textBoxMP.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxMP.Font = new System.Drawing.Font("Arial Narrow", 19.8F);
+            this.textBoxMP.Location = new System.Drawing.Point(88, 167);
+            this.textBoxMP.Name = "textBoxMP";
+            this.textBoxMP.Size = new System.Drawing.Size(472, 45);
+            this.textBoxMP.TabIndex = 4;
+            // 
+            // ValidationButton
+            // 
+            this.ValidationButton.AutoSize = true;
+            this.ValidationButton.BackColor = System.Drawing.Color.ForestGreen;
+            this.ValidationButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ValidationButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ValidationButton.Font = new System.Drawing.Font("Arial Rounded MT Bold", 19.8F);
+            this.ValidationButton.Location = new System.Drawing.Point(0, 249);
+            this.ValidationButton.Name = "ValidationButton";
+            this.ValidationButton.Size = new System.Drawing.Size(638, 79);
+            this.ValidationButton.TabIndex = 5;
+            this.ValidationButton.Text = "Valider";
+            this.ValidationButton.UseVisualStyleBackColor = false;
+            this.ValidationButton.Click += new System.EventHandler(this.ValidationButton_Click);
+            // 
+            // OcarinaBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(638, 328);
+            this.Controls.Add(this.ValidationButton);
+            this.Controls.Add(this.textBoxMP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxName);
+            this.Name = "OcarinaBox";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OcarinaBox";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxMP;
+        private System.Windows.Forms.Button ValidationButton;
+    }
+}
+
---
+++ Escape Game Online v1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Windows.Forms;
+
+
+namespace Escape_Game_Online_v1
+{
+    public partial class OcarinaBox : Form
+    {
+        public OcarinaBox()
+        {
+            InitializeComponent();
+        }
+        //--------------fonction------------------------------------
+        void LancerVideo(string PathVideo)
+        {
+            if (System.IO.File.Exists(PathVideo))
+            {
+                System.Diagnostics.Process.Start(PathVideo);   
+            }
+            else
+            {
+                MessageBox.Show("Fichier introuvable : " + PathVideo);
+                this.Show();
+            }
+        }
+        void GoToSite(string url)
+        {
+            System.Diagnostics.Process.Start(url);
+            
+        }
+        void clear()
+        {
+            textBoxMP.Text = "";
+            textBoxName.Text = "";
+        }
+        //----------------- Constantes -----------------------------
+
+        string NomUtilisateur = "";
+       
+
+        string site1 = "https://www.youtube.com";
+        string site2 = "https://www.youtube.com";
+        string site3 = "https://www.youtube.com";
+        string site4 = "https://www.youtube.com";
+        
+
+        string PathVraiVideo = "VraiVideo.mp4";
+
+        //----------------------------------------------------------
+        private void ValidationButton_Click(object sender, EventArgs e)
+        {
+            if (textBoxName.Text == NomUtilisateur)
+            {
+                switch(textBoxMP.Text)
+                {
+                    case "1":                   //mot de passe pour site 1 
+                       
+                        GoToSite(site1);
+                        MessageBox.Show("b");                       
+                        break;
+
+                    case "2":                   //mot de passe pour site 2
+                        MessageBox.Show("Trouvez la solution a cette enigme : ... ?");
+                        GoToSite(site2);
+                        GoToSite(site3);
+                        GoToSite(site4);
+                        GoToSite(site1);
+                        break;
+
+                    case "3":                   //mot de passe pour site 3
+                        MessageBox.Show("b");
+                        GoToSite(site3);
+                        break;
+
+                    case "4":                   //mot de passe pour site 4
+                        MessageBox.Show("b");
+                        GoToSite(site4);
+                        break;
+
+                    case "vrai":                   //Vrai mot de passe
+                        LancerVideo(PathVraiVideo);
+                        clear();
+                        break;
+
+                    default:
+                        MessageBox.Show("ERROR : Mauvais mot de passe");
+                        break;
+                }   
+            }
+            else
+            {
+                MessageBox.Show("ERROR : Nom utilisateur");
+            }
+        }
+       
+    }
+}
