a606d45450558dbab6a3250b45bc9313f7c1caef
AlexLuthor09 <capalex@hotmail.be>
2020-09-04 17:25:38 +0200

    acceptButton

--- Escape Game Online v1/Form1.Designer.cs
+++ Escape Game Online v1/Form1.Designer.cs
@@ -90,6 +90,7 @@
             // 
             // OcarinaBox
             // 
+            this.AcceptButton = this.ValidationButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gray;
